8ded6dc542541a9d3bb84f1cf1b516a7eb5466c7
Carolyn Castanon <caca1257@colorado.edu>
2017-02-13 11:58:22 -0700

    added if else statments

--- sketch.js
+++ sketch.js
@@ -18,20 +18,28 @@ function draw() {
   textAlign(RIGHT, TOP);
   text("sprite count: " + allSprites.length, width-10, 10);
 
-  if(allSprites.length == 50){
-    background(random(255),random(255),random(255));
+  if(allSprites.length === 50){
+    var frame = background(random(255),random(255),random(255));
     text("YOU WON!", width/2, height/2);
   }
+  else if(allSprites.length > 50){
+    var frame = background(0);
+    fill(255);
+    text("YOU LOST", width/2,height/2);
+  }
+  
 
   drawSprites();
 }
 
 function mousePressed(){
-  var spr = createSprite(mouseX, mouseY, random(10, 50), random(10,50));
+  var spr = createSprite(random(width), random(height), random(10, 50), random(10,50));
   fill(random(255),random(255),random(255));
 
   spr.velocity.x = random(-5, 5);
   spr.velocity.y = random(-5, 5);
 
 }
+
+
 
