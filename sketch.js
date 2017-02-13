db0ccad35b9e29c02a7b7caef040caa9d479e8a7
Carolyn Castanon <caca1257@colorado.edu>
2017-02-13 11:31:53 -0700

    set how many sprites

--- sketch.js
+++ sketch.js
@@ -1,18 +1,27 @@
 
+
 function setup() {
   createCanvas(800, 400);
-
 }
+
+
 function draw() {
-  background(255,255,255);
+
+  background('whitesmoke');
+
 
   fill(0);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
   text("START CLICKING", width/2, height/2);
 
   fill(0);
   textAlign(RIGHT, TOP);
   text("sprite count: " + allSprites.length, width-10, 10);
+
+  if(allSprites.length == 50){
+    background(random(255),random(255),random(255));
+    text("YOU WON!", width/2, height/2);
+  }
 
   drawSprites();
 }
